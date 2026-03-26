cacb7f0a9214b2e67558b43671a9f8d28882d8fc
Harshitha006 <136321243+Harshitha006@users.noreply.github.com>
2026-03-26 13:43:16 +0530

    Delete Backend directory

--- Backend/models/DietPlan.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DietPlanSchema = new mongoose.Schema({
-    recommendedFoods: [String],
-    restrictedFoods: [String]
-});
-
-module.exports = mongoose.model("DietPlan", DietPlanSchema);
